6db88726772153a76160358c7ce1883f29e1608d
S M Khirul Alam <ka.shagor1@gmail.com>
2024-12-17 12:14:20 +0600

    Completed dummy testimonial section

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,6 @@
 import logo from "./images/image1.jpg";
 import banner from "./images/image2.png";
-export { logo, banner };
+import testimonialOne from "./images/testimonialOne.png";
+import testimonialTwo from "./images/testimonialTwo.png";
+import quote from "./images/quote.png";
+export { logo, banner, testimonialOne, testimonialTwo, quote };
